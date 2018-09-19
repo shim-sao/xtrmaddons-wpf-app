531c6955ab7f4346e721952ac03db454fe9fba16
Laurent Barraud <contact@xtrmaddons.com>
2018-09-19 16:27:43 +0200

    Target Framework Version v4.7.1

--- XtrmAddons.Net.Application.Examples/Properties/AssemblyInfo.cs
+++ XtrmAddons.Net.Application.Examples/Properties/AssemblyInfo.cs
@@ -50,5 +50,5 @@ using System.Windows;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.18.0305.1035")]
-[assembly: AssemblyFileVersion("1.18.0305.1035")]
+[assembly: AssemblyVersion("1.18.18259.0924")]
+[assembly: AssemblyFileVersion("1.18.18259.0924")]
